dc3c1ce5e09b1d31ed00935bfcdb175bce845231
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-24 22:29:32 +0300

    Fix typos

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 # set up client SSH configuration file so that you can connect to a server
 # without typing a password and sets IdentityFile ~/.ssh/school
 
-file_line {'Sets pw auth to no':
-  path   =>'/etc/ssh/ssh_config'
+file_line { 'Sets pw auth to no':
   ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no'
 }
 
 file_line { 'Sets id file':
-  path   =>'/etc/ssh/ssh_config'
   ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school'
 }
