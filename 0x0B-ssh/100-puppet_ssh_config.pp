d53e4e7d9e03c3f30aac99ae7b444828b55f88cc
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-24 22:27:20 +0300

    Add path parameter

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,14 @@
 # set up client SSH configuration file so that you can connect to a server
 # without typing a password and sets IdentityFile ~/.ssh/school
 
-file_line { '/etc/ssh/ssh_config':
+file_line {'Sets pw auth to no':
+  path   =>'/etc/ssh/ssh_config'
   ensure => 'present',
   line   => '    PasswordAuthentication no'
 }
 
-file_line { '/etc/ssh/ssh_config':
+file_line { 'Sets id file':
+  path   =>'/etc/ssh/ssh_config'
   ensure => 'present',
   line   => '    IdentityFile ~/.ssh/school'
 }
