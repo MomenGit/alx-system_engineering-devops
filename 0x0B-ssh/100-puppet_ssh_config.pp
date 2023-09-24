c4dfa99c3c8b280f9c32a0831e34d518073e745c
Moamen Majdy <51290928+MomenGit@users.noreply.github.com>
2023-09-24 22:20:28 +0300

    Write 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,2 +1,12 @@
 # set up client SSH configuration file so that you can connect to a server
-# without typing a password
+# without typing a password and sets IdentityFile ~/.ssh/school
+
+file_line { '/etc/ssh/ssh_config':
+  ensure => 'present',
+  line   => '    PasswordAuthentication no'
+}
+
+file_line { '/etc/ssh/ssh_config':
+  ensure => 'present',
+  line   => '    IdentityFile ~/.ssh/school'
+}
